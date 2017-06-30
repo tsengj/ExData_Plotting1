b8351314d36a64dff0af99fe66dd7b58bb6d733f
James Tseng <james.tseng00@gmail.com>
2017-06-30 22:36:12 +1000

    complete

--- jt_plot.R
+++ jt_plot.R
@@ -9,7 +9,7 @@ if(!file.exists("exdata-data-household_power_consumption.zip")) {
   unlink(temp)
 }
 
-d1<- read.table(file, 
+df<- read.table(file, 
                 sep=";",
                 header = T,
                 stringsAsFactors = F)
@@ -20,7 +20,7 @@ file.remove('household_power_consumption.txt')
 
 #format dataframe
 
-d1 <- d1 %>% mutate(DTime = as.POSIXct(paste(Date, Time), format="%d/%m/%Y %H:%M"),
+df <- df %>% mutate(timestamp = as.POSIXct(paste(Date, Time), format="%d/%m/%Y %H:%M"),
                     Date = as.Date(Date,format="%d/%m/%Y"),
                     Time = as.POSIXct(Time, format="%H:%M:%S"),
                     Global_active_power = as.numeric(Global_active_power),
@@ -34,7 +34,7 @@ d1 <- d1 %>% mutate(DTime = as.POSIXct(paste(Date, Time), format="%d/%m/%Y %H:%M
 
 #plot 1
 plot1 <- function() {
-  hist(d1$Global_active_power,
+  hist(df$Global_active_power,
        col='red',
        xlab="Global Active Power (kw)",
        ylab="Frequency",
@@ -45,10 +45,47 @@ plot1 <- function() {
 }
 plot1()
 
-plot1 <- function() {
+#plot 2
+plot2 <- function() {
   plot(df$timestamp,df$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
   dev.copy(png, file="plot2.png", width=480, height=480)
   dev.off()
   cat("plot2.png has been saved in", getwd())
 }
-plot1()
+plot2()
+
+#plot 3
+plot3 <- function() {
+  plot(df$timestamp,df$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+  lines(df$timestamp,df$Sub_metering_2,col="red")
+  lines(df$timestamp,df$Sub_metering_3,col="blue")
+  legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty=c(1,1), lwd=c(1,1))
+  dev.copy(png, file="plot3.png", width=480, height=480)
+  dev.off()
+  cat("plot3.png has been saved in", getwd())
+}
+plot3()
+
+plot4 <- function() {
+  par(mfrow=c(2,2))
+  
+  ##PLOT 1
+  plot(df$timestamp,df$Global_active_power, type="l", xlab="", ylab="Global Active Power")
+  ##PLOT 2
+  plot(df$timestamp,df$Voltage, type="l", xlab="datetime", ylab="Voltage")
+  
+  ##PLOT 3
+  plot(df$timestamp,df$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+  lines(df$timestamp,df$Sub_metering_2,col="red")
+  lines(df$timestamp,df$Sub_metering_3,col="blue")
+  legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty=c(1,1), bty="n", cex=.5) #bty removes the box, cex shrinks the text, spacing added after labels so it renders correctly
+  
+  #PLOT 4
+  plot(df$timestamp,df$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
+  
+  #OUTPUT
+  dev.copy(png, file="plot4.png", width=480, height=480)
+  dev.off()
+  cat("plot4.png has been saved in", getwd())
+}
+plot4()
